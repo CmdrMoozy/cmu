782b7594700babef3fd3943ff5d2f5f7b428950c
Axel Rasmussen <axel.rasmussen1@gmail.com>
2016-07-03 00:40:09 -0700

    Add an additional warning suppression for building Protobufs.

--- cmake/SetFlags.cmake
+++ cmake/SetFlags.cmake
@@ -44,6 +44,7 @@ function(cmuSetCompileFlags)
 		set(F_WARN "${F_WARN} -Wno-missing-variable-declarations")
 		set(F_WARN "${F_WARN} -Wno-shorten-64-to-32")
 		set(F_WARN "${F_WARN} -Wno-unused-macros")
+		set(F_WARN "${F_WARN} -Wno-extended-offsetof")
 
 		set(F_DBG "${F_WARN} -std=c++1y -g -O0 -fno-omit-frame-pointer")
 		set(F_DBG "${F_DBG} -fstack-protector-strong")
